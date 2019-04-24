58c0769366bddd3c57d52a128c75060daaccdf3a
shann <shann@DESKTOP-BRN2A77.sduhsd.lan>
2019-04-24 11:43:07 -0700

    Pong

--- src/Wall.java
+++ src/Wall.java
@@ -1,13 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author shann
- */
-public class Wall {
-    
+public class Wall extends Block
+{
+	private int width, height;
+	
+	public Wall()
+	{
+		setPos(0, 0);
+		width=800;
+		height=600;
+	}
+	
+	public Wall(int wid, int hei)
+	{
+		setPos(0, 0);
+		width=wid;
+		height=hei;
+	}
+	
+	public void setWidthHeight(int wid, int hei)
+	{
+		width=wid;
+		height=hei;
+	}
+	
+	public int getWidth()
+	{
+		return width;
+	}
+	
+	public int getHeight()
+	{
+		return height;
+	}
 }
